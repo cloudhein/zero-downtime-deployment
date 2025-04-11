edb65191f88e99a31877572a39e9c8fb7b90a188
NandaHein <devopsnandahein28@gmail.com>
2025-04-11 15:03:10 +0700

    change backend logic to Hola

--- express-backend/index.js
+++ express-backend/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 
 app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "Bonjour" });
+  res.json({ message: "Hola" });
 });
 
 const server = app.listen(port, () => {
--- express-backend/index.test.js
+++ express-backend/index.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const { app, server } = require("./index");
 
 describe("GET /api/v1/hello", () => {
-  test('should return a JSON response with the message "Bonjour"', async () => {
+  test('should return a JSON response with the message "Hola"', async () => {
     const response = await request(app).get("/api/v1/hello").expect(200);
 
-    expect(response.body.message).toBe("Bonjour");
+    expect(response.body.message).toBe("Hola");
   });
 
   test('should not return a JSON response with the message "hi"', async () => {
